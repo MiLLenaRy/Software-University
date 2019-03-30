92dc92caf0ad0d6f81e0ff941dc2ee12f4228c68
Andrey Andreev <millenary1989@gmail.com>
2019-03-30 13:40:29 +0200

    Delete items

---
+++ Technologies Fundamentals C#/01 Intro and Basic Syntax-Exercise/07 Vending Machine/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace _7._Vending_M
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = (Console.ReadLine());
+
+            double balance = 0;
+            while (input != "Start")
+            {
+                double coint = double.Parse(input);
+
+                if (coint == 0.1 ||
+                    coint == 0.2 ||
+                    coint == 0.5 ||
+                    coint == 1 ||
+                    coint == 2)
+                {
+                    balance += coint;
+                }
+                else
+                {
+                    Console.WriteLine($"Cannot accept {coint}");
+                }
+
+                input = Console.ReadLine();
+            }
+            input = Console.ReadLine();
+            while (input != "End")
+            {
+                double productPrice = 0;
+
+                switch (input)
+                {
+                    case "Nuts":
+                        productPrice = 2;
+                        break;
+                    case "Water":
+                        productPrice = 0.7;
+                        break;
+                    case "Crisps":
+                        productPrice = 1.5;
+                        break;
+                    case "Soda":
+                        productPrice = 0.8;
+                        break;
+                    case "Coke":
+                        productPrice = 1;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid product");
+                        break;
+                }
+                if (balance >= productPrice && productPrice > 0)
+                {
+                    string productToLower = input.ToLower();
+                    Console.WriteLine($"Purchased {productToLower}");
+                    balance -= productPrice;
+                }
+                else if (balance < productPrice && productPrice > 0)
+                {
+                    Console.WriteLine("Sorry, not enough money");
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Change: {balance:F2}");
+
+        }
+    }
+}
---
+++ Technologies Fundamentals C#/01 Intro and Basic Syntax-Exercise/08 Triangle of Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _8._Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int a = 1; a <= i; a++)
+                {
+                    Console.Write(i + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
