d7e510d4942902bce6230ac9e87333b1d69fa71a
Andrey Andreev <millenary1989@gmail.com>
2019-03-31 20:41:37 +0300

    Add exercise

---
+++ Technologies Fundamentals C#/03 Arrays-Exercise/07 Max Sequence of Equal Elements/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07_Max_Sequence_of_Equal_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            int bigerRow = 1;
+            int numberHolder = numbers[0];
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                int counter = 1;
+                for (int b = i + 1; b < numbers.Count; b++)
+                {
+                    if (numbers[i] == numbers[b])
+                    {
+                        counter++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                    if (counter > bigerRow)
+                    {
+                        bigerRow = counter;
+                        numberHolder = numbers[i];
+                    }
+
+                }
+            }
+            int[] bestRow = new int[bigerRow];
+            for (int max = 0; max < bigerRow; max++)
+            {
+                bestRow[max] = numberHolder;
+            }
+            Console.WriteLine(string.Join(' ', bestRow));
+        }
+    }
+}
---
+++ Technologies Fundamentals C#/03 Arrays-Exercise/08 Magic Sum/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08_Magic_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int sum = int.Parse(Console.ReadLine());
+
+            for (int a = 0; a < numbers.Count; a++)
+            {
+                int sumNumbers = numbers[a];
+                for (int b = a + 1; b < numbers.Count; b++)
+                {
+                    sumNumbers += numbers[b];
+                    if (sumNumbers == sum)
+                    {
+                        Console.WriteLine($"{numbers[a]} {numbers[b]}");
+                        sumNumbers = numbers[a];
+                    }
+                    else
+                    {
+                        sumNumbers = numbers[a];
+                    }
+                }
+            }
+        }
+    }
+}
