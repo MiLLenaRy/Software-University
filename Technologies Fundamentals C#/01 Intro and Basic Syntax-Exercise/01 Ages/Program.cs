c0f0123ac5ad1ca6b2b9f38738ecf496f184f78c
Andrey Andreev <millenary1989@gmail.com>
2019-03-31 16:39:41 +0300

    Add exercise

---
+++ Technologies Fundamentals C#/01 Intro and Basic Syntax-Exercise/01 Ages/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _01_Ages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            if (input <= 2)
+            {
+                Console.WriteLine("baby");
+            }
+            else if (input <= 13)
+            {
+                Console.WriteLine("child");
+            }
+            else if (input <= 19)
+            {
+                Console.WriteLine("teenager");
+            }
+            else if (input <= 65)
+            {
+                Console.WriteLine("adult");
+            }
+            else
+            {
+                Console.WriteLine("elder");
+            }
+        }
+    }
+}
---
+++ Technologies Fundamentals C#/01 Intro and Basic Syntax-Exercise/09 Padawa/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _09_Padawa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double money = double.Parse(Console.ReadLine());
+            int students = int.Parse(Console.ReadLine());
+            double priceOfLightsaber = double.Parse(Console.ReadLine());
+            double priceOfRobe = double.Parse(Console.ReadLine());
+            double priceOfBelts = double.Parse(Console.ReadLine());
+
+            double freeBelts = students / 6;
+            double finalPrice = priceOfLightsaber * Math.Ceiling(students * 1.10) + (priceOfRobe * students) + priceOfBelts * (students - Math.Ceiling(freeBelts));
+
+            if (finalPrice <= money)
+            {
+                Console.WriteLine($"The money is enough - it would cost {finalPrice:F2}lv.");
+            }
+            else
+            {
+                Console.WriteLine($"Ivan Cho will need {finalPrice - money:F2}lv more.");
+            }
+        }
+    }
+}
