e6e465d139fe908aa7d7442765bf5bacd1419847
Andrey Andreev <millenary1989@gmail.com>
2019-04-04 19:10:49 +0300

    Create folder & Add exercise

---
+++ Technologies Fundamentals C#/Lab - Text Processing and Regular Expressions/01. Reverse Strings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _01._Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            while (word!="end")
+            {
+                string revesedWord = string.Empty;
+                for (int i = word.Length - 1; i >= 0; i--)              
+                {
+                    revesedWord += word[i];
+                }
+                Console.WriteLine($"{word} = {revesedWord}");
+                word = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Technologies Fundamentals C#/Lab - Text Processing and Regular Expressions/01. Reverse Strings/README.md
@@ -0,0 +1,2 @@
+⦁	Reverse Strings
+You will be given series of strings until you receive an "end" command. Write a program that reverses strings and prints each pair on a separate line in the format "{word} = {reversed word}".
