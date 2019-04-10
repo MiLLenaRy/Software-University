3ddf3713439febeafccd75e1612ddafd5ff02bf6
Andrey Andreev <millenary1989@gmail.com>
2019-04-10 20:47:03 +0300

    Add exam exercise

---
+++ Technologies Fundamentals C#/Exams/02. Deciphering/02. Deciphering/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _02._Deciphering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string decript = Console.ReadLine();
+            string[] words = Console.ReadLine().Split(' ').ToArray();
+
+            StringBuilder resoult = new StringBuilder();
+            for (int a = 0; a < decript.Length; a++)
+            {
+                if (decript[a]>='d' && decript[a]<'~' || decript[a] == '#')
+                {
+                    resoult.Append((char)(decript[a] - 3));
+                }
+                else
+                {
+                    Console.WriteLine("This is not the book you are looking for.");
+                    return;
+                }
+            }
+            resoult.Replace(words[0], words[1]);
+            Console.WriteLine(resoult);
+        }
+    }
+}
