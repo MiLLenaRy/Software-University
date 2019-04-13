9f4d241cb820be924cf59ad5ef3f60f20bfa8c90
Andrey Andreev <millenary1989@gmail.com>
2019-04-13 18:41:35 +0300

    Add exercises

---
+++ Technologies Fundamentals C#/Exams/01. Vapor Winter Sale/01. Vapor Winter Sale/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _01._Vapor_Winter_Sale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> command = Console.ReadLine().Split(", ").ToList();
+
+            var gameAndPrice = new Dictionary<string,double>();
+            var gameAndDLC = new Dictionary<string, string>();
+
+            for (int a = 0; a < command.Count; a++)
+            {
+                if (command[a].Contains(':'))
+                {
+                    string[] split = command[a].Split(':');
+                    string nameOfGame = split[0];
+                    string DLC = split[1];
+                    if (gameAndPrice.ContainsKey(nameOfGame))
+                    {
+                        gameAndDLC.Add(nameOfGame,DLC);
+
+                        double increasedPrice = gameAndPrice[nameOfGame] *1.2;
+                        //if decimal 1.20m
+                        gameAndPrice[nameOfGame] = increasedPrice;
+                        
+                    }
+                }
+                else
+                {
+                    string[] split = command[a].Split('-');
+                    string nameOfGame = split[0];
+                    double price = double.Parse(split[1]);
+                    if (!gameAndPrice.ContainsKey(nameOfGame))
+                    {
+                        gameAndPrice.Add(nameOfGame, price);
+                    }
+                }      
+            }
+            var gamesAndLowerPriceWithoutDlc = new Dictionary<string, double>();
+            var gamesAndLowerPriceWithDlc = new Dictionary<string, double>();
+
+            foreach (var game in gameAndPrice)
+            {
+                string gameName = game.Key;
+                double price = game.Value;
+
+                if (!gameAndDLC.ContainsKey(gameName))
+                {
+                    double lowerPrice = price - (price * 0.2);
+                    gamesAndLowerPriceWithoutDlc.Add(gameName, lowerPrice);
+                }
+                else
+                {
+                    double lowerPrice = price - (price * 0.5);
+                    gamesAndLowerPriceWithDlc.Add(gameName, lowerPrice);
+                }
+            }
+            foreach (var kvp in gamesAndLowerPriceWithDlc.OrderBy(x=>x.Value))
+            {
+                string name = kvp.Key;
+                double price = kvp.Value;
+                Console.WriteLine($"{name} - {gameAndDLC[kvp.Key]} - {price:f2}");
+            }
+            foreach (var kvp in gamesAndLowerPriceWithoutDlc.OrderByDescending(x=>x.Value))
+            {
+                string name = kvp.Key;
+                double price = kvp.Value;
+                Console.WriteLine($"{name} - {price:F2}");
+            }
+
+        }
+    }
+}
---
+++ Technologies Fundamentals C#/Exams/02. Activation Keys/02. Activation Keys/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _02._Activation_Keys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split('&');
+
+            string decript = @"[A-Za-z-0-9]{16,25}";
+            var newCode = new StringBuilder();
+
+            List<string> passwords = new List<string>();
+
+
+
+            for (int a = 0; a < input.Length; a++)
+            {
+                Match cheker = Regex.Match(input[a], decript);
+
+                if (input[a].Length == cheker.Length)
+                {
+                    passwords.Add(input[a]);
+                }
+            }
+
+            for (int x = 0; x < passwords.Count; x++)
+            {
+                string word = passwords[x].ToUpper();
+
+                for (int y = 0; y < word.Length; y++)
+                {
+                    if (word.Length == 16)
+                    {
+                        if (y % 4 == 0 && y != 0)
+                        {
+                            newCode.Append('-');
+                        }
+                        if (char.IsDigit(word[y]))
+                        {
+                            int number = word[y] - 48;
+                            number = 9 - number;
+                            newCode.Append(number);
+                        }
+                        else
+                        {
+                            newCode.Append(word[y]);
+                        }
+                    }
+                    if (word.Length == 25)
+                    {
+                        if (y % 5 == 0 && y != 0)
+                        {
+                            newCode.Append('-');
+                        }
+                        if (char.IsDigit(word[y]))
+                        {
+                            int number = word[y] - 48;
+                            number = 9 - number;
+                            newCode.Append(number);
+                        }
+                        else
+                        {
+                            newCode.Append(word[y]);
+                        }
+                    }
+
+                }
+                if (passwords.Count - 1 != x)
+                {
+                    newCode.Append(", ");
+
+                }
+            }
+            Console.WriteLine(newCode);
+        }
+
+    }
+}
+
