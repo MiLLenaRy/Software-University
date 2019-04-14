4546a0e95defc0a837a03efdcc5eca0ef0e0396e
Andrey Andreev <millenary1989@gmail.com>
2019-04-14 20:08:37 +0300

    Add exam exercises

---
+++ Technologies Fundamentals C#/Exams/01. Arriving in Kathmandu/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Problem_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command;
+            string decript = @"([!]*[#]*[@]*[$]*[A-Z]*[a-z]*[?]*[0-9]*)+";
+
+            while ((command=Console.ReadLine())!= "Last note")
+            {                
+                List<string> array = command.Split("=").ToList();
+                string firstWord = array[0];
+                Match cheker = Regex.Match(firstWord, decript);
+
+                if (firstWord.Length==cheker.Length)
+                {
+                    string number = string.Empty;
+                    string[] secondArray = array[1].Split("<<").ToArray();
+                    int counter = 0;
+                    for (int x = 0; x < secondArray[0].Length; x++)
+                    {
+                             
+                        if (char.IsDigit(secondArray[0][x]))
+                        {
+                            counter++;
+                            number += secondArray[0][x];
+                        }
+                        
+                    }                   
+                    int numbers = 0;
+                    if (counter==secondArray[0].Length)
+                    {
+                        numbers = int.Parse(number);
+                        
+                    }                 
+                    if (secondArray[1].Length==numbers)
+                    {
+                        var finalResoult = new StringBuilder();
+                        for (int i = 0; i < firstWord.Length; i++)
+                        {
+                            if (char.IsLetterOrDigit(firstWord[i]))
+                            {
+                                finalResoult.Append(firstWord[i]);
+                            }
+                        }
+                        finalResoult.Append(" -> ");
+                        for (int i = 0; i < secondArray[1].Length; i++)
+                        {
+                            finalResoult.Append(secondArray[1][i]);
+                        }
+                        Console.WriteLine("Coordinates found! " + finalResoult);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nothing found!");
+                    }
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Nothing found!");
+                }            
+            }
+        }
+    }
+}
---
+++ Technologies Fundamentals C#/Exams/02. On the Way to Annapurna/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Problem_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command;
+
+            var dict = new Dictionary<string, List<string>>();
+
+            while ((command=Console.ReadLine())!="END")
+            {
+                string[] array = command.Split("->").ToArray();
+                string option = array[0];
+                string type = array[1];
+
+                
+                if (option=="Add")
+                {
+                    List<string> lastLine = array[2].Split(',').ToList();
+                    
+                    if (!dict.ContainsKey(type))
+                    {
+                        dict.Add(type, new List<string>());
+                    }
+                    for (int b = 0; b < lastLine.Count; b++)
+                    {
+                        dict[type].Add("<<"+lastLine[b]+">>");
+                    }                    
+                }
+                else if(option == "Remove")
+                {                
+                    dict.Remove(type);
+                }
+            }
+            Console.WriteLine("Stores list:");
+            foreach (var item in dict.OrderByDescending(x=>x.Value.Count).ThenByDescending(x=>x.Key))
+            {
+                Console.WriteLine(item.Key);
+                foreach (var name in item.Value)
+                {
+                    Console.WriteLine(name);
+                }
+            }
+        }
+    }
+}
