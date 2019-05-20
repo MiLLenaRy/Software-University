a4b00729d00d121bbefa5c79829a2b3ddd7f069a
Andrey Andreev <millenary1989@gmail.com>
2019-05-20 15:02:55 +0300

    Add exercises

---
+++ C# Advanced - May 2019/04. EXERCISE STACKS AND QUEUES/09 Simple Text Editor/StartUp.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _09_Simple_Text_Editor
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Stack<string> stackOfText = new Stack<string>();
+            StringBuilder text = new StringBuilder();
+
+            int counts = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < counts; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string command = input[0];
+
+                if (command == "1")
+                {
+                    stackOfText.Push(text.ToString());
+                    text.Append(input[1]);
+                }
+                else if (command == "2")
+                {
+                    int index = int.Parse(input[1]);
+                    stackOfText.Push(text.ToString());
+                    text.Remove(text.Length - index, index);
+                }
+                else if (command == "3")
+                {
+                    int index = int.Parse(input[1]);
+                    Console.WriteLine(text[index - 1]);
+                }
+                else if (command == "4")
+                {
+                    text.Clear();
+                    text.Append(stackOfText.Pop());
+                }
+            }
+
+        }
+    }
+}
---
+++ C# Advanced - May 2019/04. EXERCISE STACKS AND QUEUES/10.  Crossroads/StartUp.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+namespace _10.__Crossroads
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Queue<string> queueOfCars = new Queue<string>();
+
+            int greenLinghtDuration = int.Parse(Console.ReadLine());
+            int freeWindowDuration = int.Parse(Console.ReadLine());
+
+            string input = Console.ReadLine();
+
+            bool isHitted = false;
+            string hittedCarName = string.Empty;
+            char hittedSymbol = '\0';
+            int totalCars = 0;
+
+            while (input != "END")
+            {
+                if (input != "green")
+                {
+                    queueOfCars.Enqueue(input);
+                    input = Console.ReadLine();
+                    continue;
+                }
+
+                int currentGreenLigntDuration = greenLinghtDuration;
+
+                while (currentGreenLigntDuration > 0 && queueOfCars.Count > 0)
+                {
+                    string carName = queueOfCars.Dequeue();
+                    int carLenght = carName.Length;
+
+                    if (currentGreenLigntDuration - carLenght >= 0)
+                    {
+                        currentGreenLigntDuration -= carLenght;
+                        totalCars++;
+                    }
+                    else
+                    {
+                        currentGreenLigntDuration += freeWindowDuration;
+
+                        if (currentGreenLigntDuration - carLenght >= 0)
+                        {
+                            totalCars++;
+                            break;
+                        }
+                        else
+                        {
+                            isHitted = true;
+                            hittedCarName = carName;
+                            hittedSymbol = carName[currentGreenLigntDuration];
+                            break;
+                        }
+                    }
+                }
+
+                if (isHitted)
+                {
+                    break;
+                }
+                input = Console.ReadLine();
+            }
+
+            if (isHitted)
+            {
+                Console.WriteLine("A crash happened!");
+                Console.WriteLine($"{hittedCarName} was hit at {hittedSymbol}.");
+            }
+            else
+            {
+                Console.WriteLine("Everyone is safe.");
+                Console.WriteLine($"{totalCars} total cars passed the crossroads.");
+            }
+        }
+    }
+}
